f1f0d7f6ebf94641f0a252b9e964929bfe28f25b
Ray <RNova94037@Gmail.com>
2016-01-30 23:23:09 -0800

    [WIP]sign-up: misc comments.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
-#
+# About this controller:
 class WelcomeController < ApplicationController
+  # before_action: whatever method you put here will be performed before the
+  # specifed actions. before_actions are executed in the list order.
+
+  # A frequent practice is to place the standard CRUD actions in each controller
+  # in the following order: index, show, new, edit, create, update and destroy.
   def index
   end
 end
